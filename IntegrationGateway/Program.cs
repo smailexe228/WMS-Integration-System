087d3938e759995016e0ad37e12b7e7e5912ecd5
harkygit <nikita702702@gmail.com>
2026-06-05 19:01:24 +0300

    fix ProductService Program

--- IntegrationGateway/Program.cs
+++ IntegrationGateway/Program.cs
@@ -1,6 +1,27 @@
+using IntegrationGateway.Adapters;
+using IntegrationGateway.DTOs;
+using IntegrationGateway.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(typeof(SupplyToStockProfile));
+
+builder.Services.AddScoped<SupplyIntegrationService>();
+
+builder.Services.AddHttpClient<InventoryAdapter>(client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5003");
+});
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 
